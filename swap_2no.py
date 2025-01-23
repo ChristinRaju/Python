f05913aa5d77f6f2ecb5df5a70a86a9b531bd008
ChristinRaju <2006christinraju@gmail.com>
2025-01-23 18:34:01 +0530

    Add files via upload

---
+++ swap_2no.py
@@ -0,0 +1,25 @@
+#Write a python program to swap two numbers with and without using temp
+
+# Swapping with a temporary variable
+
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: ', x)
+print('The value of y after swapping: ', y)
+
+# Swapping without a temporary variable
+
+x = int(input('Enter value of x: '))
+y = int(input('Enter value of y: '))
+
+x = x + y
+y = x - y
+x = x - y
+
+print('The value of x after swapping: ', x)
+print('The value of y after swapping: ', y)

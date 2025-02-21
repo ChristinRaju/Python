c72891c2eff992c351769e49c529a3f97e33f4cf
ChristinRaju <2006christinraju@gmail.com>
2025-02-21 10:22:35 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,10 @@
-# Get input from user and convert to integer
 num = int(input("Enter a number: "))
+n = num
 
-# Initialize variables
 factorial = 1
 
-# Calculate factorial using while loop
 while num > 0:
     factorial *= num
     num -= 1
 
-# Print the result
-print("The factorial is", factorial)
+print(f"The factorial of {n} is {factorial}")
